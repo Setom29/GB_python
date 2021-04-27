9ee2c1c43ec0514084230cf23ada28ca7b830178
s.tolstov <sergeymtolstov35@gmail.com>
2021-04-27 23:37:08 +0300

    Homework №1 done

---
+++ first_ex.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+c_string = ' - strings'
+d_string = input('Введите строку: ')
+e_string = input('Введите еще одну строку: ')
+f = int(input('Введите число: '))
+print(d_string + ' ' + 'and' + ' ' + e_string + c_string)
+print(type(f))
