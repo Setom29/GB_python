f04febd9af483262509bf71455b4e9bc0c879621
Setom29 <sergeymtolstov35@gmail.com>
2021-07-16 12:29:36 +0300

    Homework №4 done.
    vk_lesson4.sql - main script.
    vk_dump.sql - mysqldump of vk database with some mock data
    other files - mock data

---
+++ SQL/Lesson4/media_types.sql
@@ -0,0 +1,5 @@
+use vk;
+insert into media_types (id, name) values (1, 'Proactive');
+insert into media_types (id, name) values (2, 'synergy');
+insert into media_types (id, name) values (3, 'time-frame');
+insert into media_types (id, name) values (4, 'Object-based');
